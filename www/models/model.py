ce5fb73cd9663279e410c624a21c8c3b760aca2b
D4vidz <zengzhiyuan1@qq.com>
2018-06-22 08:42:21 +0800

    orm类搭建

--- www/models/model.py
+++ www/models/model.py
@@ -4,3 +4,28 @@
 
 class ModelMetaclass(type):
     pass
+
+
+class Model(dict, metaclass=ModelMetaclass):
+    pass
+
+
+class Field(object):
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class StringField(Field):
+    pass
+
+
+class IntegerField(Field):
+    pass
+
+
+class User(Model):
+    pass

5b6444fa78703bd6d4dfb30f9108494b55e17b1e
D4vidz <zengzhiyuan1@qq.com>
2018-11-08 21:12:36 +0800

    提交至可以开发api

---
+++ www/apis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@name: apis.py
+@author: Aeiou
+@time: 18-11-8 上午9:46
+"""
---
+++ www/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@name: config.py
+@author: Aeiou
+@time: 18-11-8 上午11:16
+"""
